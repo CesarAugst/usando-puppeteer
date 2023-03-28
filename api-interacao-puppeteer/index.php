e24a0200f27a76e1268e9199a45022ac4b5fd3dc
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2023-03-28 13:18:30 -0300

    testando com $_REQUEST

--- api-interacao-puppeteer/index.php
+++ api-interacao-puppeteer/index.php
@@ -4,7 +4,7 @@ $initial_time_execution = time();
 //recebendo array de urls via body_params
 $url_from_get = json_decode(file_get_contents('php://input'));
 //reescrita pois em producao nao pegou na com a linha anterior (ambiente dev 7.4 e em prod 8.2)
-$url_from_get = $_REQUEST[0];
+$url_from_get = $_REQUEST;
 die(var_dump($url_from_get));
 //se recebeu o array
 if($url_from_get):
