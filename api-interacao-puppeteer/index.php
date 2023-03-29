68a0307bd383117ea3baf4aed437e3f8e2d6899f
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2023-03-29 10:13:21 -0300

    depuracao dados recebidos por body no php

--- api-interacao-puppeteer/index.php
+++ api-interacao-puppeteer/index.php
@@ -3,6 +3,7 @@
 $initial_time_execution = time();
 //recebendo array de urls via body_params
 $url_from_get = json_decode(file_get_contents('php://input'));
+die(var_dump($url_from_get));
 //se recebeu o array
 if($url_from_get):
     //gera o arquivo txt com base no rray de urls recebidas
