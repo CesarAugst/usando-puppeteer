ca6513b70f007638db2af079e36c0b3d8720b79b
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2023-03-30 15:10:05 -0300

    descriçaõ de remove_files

--- puppeteer/utils/f_remove_file.js
+++ puppeteer/utils/f_remove_file.js
@@ -2,7 +2,7 @@
 const fs = require("fs");
 module.exports = {
 //desc: remove arquivo ao receber caminho
-//params: (number) quantidade de milisegundos que ficara esperando
+//params: (string)  caminho do arquivo com o nome
 //return: nenhum
     remove_file(path_file) {
         //abre a tentativa
